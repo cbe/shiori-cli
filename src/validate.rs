5b812bfe0c8b0f3dfd8e63bf93758cfe3155e3ff
cbe <cbe@users.noreply.github.com>
2023-09-24 15:43:47 +0200

    Satisfy clippy by being more rusty
    https://rust-lang.github.io/rust-clippy/master/index.html#/needless_return

--- src/validate.rs
+++ src/validate.rs
@@ -4,11 +4,7 @@ use crate::structs::shiori_cli::LocalCache;
 
 pub fn check_logged_in(persist: &DiskPersist<LocalCache>) -> bool {
     match persist.read().unwrap() {
-        Some(local_cache) => {
-            return !local_cache.session_id.is_empty();
-        }
-        None => {
-            return false;
-        }
-    };
+        Some(local_cache) => !local_cache.session_id.is_empty(),
+        None => false,
+    }
 }
